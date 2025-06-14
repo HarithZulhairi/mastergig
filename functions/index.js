6482053586193b45fac09b9dbb8ee2db596efbf2
IkhwanBadzli <165304525+IkhwanBadzli@users.noreply.github.com>
2025-06-15 00:36:21 +0800

    c

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,29 @@
-// functions/index.js
 const functions = require('firebase-functions');
 const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const cors = require('cors')({ origin: true });
 
-exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(data.amount * 100), // Convert to cents
-      currency: data.currency,
-    });
-    return { clientSecret: paymentIntent.client_secret };
-  } catch (e) {
-    throw new functions.https.HttpsError('internal', e.message);
-  }
+const stripe = Stripe(functions.config().stripe.secret);
+
+exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    if (req.method !== 'POST') {
+      return res.status(405).send('Method Not Allowed');
+    }
+
+    try {
+      const { amount, currency } = req.body;
+      
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: Math.round(amount * 100), // Convert to cents
+        currency: currency.toLowerCase(),
+      });
+
+      res.status(200).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (err) {
+      console.error('Error creating payment intent:', err);
+      res.status(500).json({ error: err.message });
+    }
+  });
 });

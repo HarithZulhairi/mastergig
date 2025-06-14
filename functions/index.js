5de2fe5593b6d5c9899b127b41e19bd4fa22c86f
IkhwanBadzli <165304525+IkhwanBadzli@users.noreply.github.com>
2025-06-14 23:56:51 +0800

    Bad Cuba Integrate Stripe

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+exports.createPaymentIntent = functions.https.onRequest(async (req, res) => {
+  try {
+    const { amount, currency } = req.body;
+    
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100), // Convert to cents
+      currency: currency.toLowerCase(),
+    });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret
+    });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
